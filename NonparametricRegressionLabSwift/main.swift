5ccbc957bf633af3b9e6c5087afb6c202f0e2f01
Terky <artemab@outlook.com>
2019-09-21 21:51:02 +0300

    Initial Commit

---
+++ NonparametricRegressionLabSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NonparametricRegressionLabSwift
+//
+//  Created by Артём Бурмистров on 9/21/19.
+//  Copyright © 2019 Артём Бурмистров. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
